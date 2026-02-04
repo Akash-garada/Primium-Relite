42908ae093df98d024c3faa9c944f270263b33e5
Akash-garada <onlinelozy@gmail.com>
2026-02-04 20:13:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function orderNow(productName, price) {
-  const phoneNumber = "91XXXXXXXXXX"; // apna WhatsApp number
+  const phoneNumber = "917008815216"; // apna WhatsApp number
   const message = `Hi Relite Clothing, I want to order ${productName} priced at ${price}`;
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
